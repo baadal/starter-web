14307ffd7f2aaffac2feaf10e0dac600d096a99d
Abhishek Raj <abhi@raj.me>
2020-10-13 19:03:05 +0530

    Setup express

--- web/index.ts
+++ web/index.ts
@@ -1,5 +1,45 @@
-const server = () => {
-  console.log('Hello from server!');
-}
+import express from 'express';
+import fs from 'fs';
 
-server();
+import { checkProd } from 'starter/utils/env';
+
+const app = express();
+const PORT = 3000;
+
+const isProd = checkProd();
+
+// hide powered by express
+app.disable('x-powered-by');
+
+// disallow caching of JS/CSS bundles in dev mode
+app.use((req, res, next) => {
+  if (!isProd && req.path.match(/\.(js|css)$/i)) {
+    // Ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#preventing_caching
+    res.setHeader('Cache-Control', 'no-store, max-age=0');
+  }
+  return next();
+});
+
+// enable CORS for JS/font files
+app.use((req, res, next) => {
+  if (req.path.match(/\.(js|ttf|woff2?)$/i)) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  }
+  return next();
+});
+
+// serve static assets
+app.use(express.static('build/public'));
+
+app.get('/*', (req, res) => {
+  const filePath = process.cwd() + `/build/public/index.html`;
+  if (fs.existsSync(filePath)) {
+    res.sendFile(filePath);
+  } else {
+    res.send('Please refresh!');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`\nApp running at port ${PORT} 😎\n`);
+});
