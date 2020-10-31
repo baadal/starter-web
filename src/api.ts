049b7761b52d4060e72e2a00cd9e664c3b815b65
Abhishek Raj <abhi@raj.me>
2020-10-31 17:13:05 +0530

    Setup state store [rxjs]

--- src/api.ts
+++ src/api.ts
@@ -43,6 +43,15 @@ const cssInJsDemo = {
   },
 };
 
+const storeInfo = () => ({
+  title: '<i>Demo:</i> State Store',
+  timestamp: new Date().toISOString(),
+  seo: {
+    title: 'Demo: State Store',
+    description: 'A demonstration for using state store',
+  },
+});
+
 const homeInfo = {
   title: 'My Starter App',
   description: 'The modern way!',
@@ -116,6 +125,7 @@ const sendResponse = (req: express.Request, res: express.Response, data: any) =>
 app.get('/v1/data/about', (req, res) => sendResponse(req, res, aboutInfo));
 app.get('/v1/data/demo/css-styles', (req, res) => sendResponse(req, res, cssStylesDemoInfo));
 app.get('/v1/data/demo/css-in-js', (req, res) => sendResponse(req, res, cssInJsDemo));
+app.get('/v1/data/demo/state-store', (req, res) => sendResponse(req, res, storeInfo()));
 app.get('/v1/data/home', (req, res) => sendResponse(req, res, homeInfo));
 app.get('/v1/data/not-found', (req, res) => sendResponse(req, res, notFoundInfo));
 
