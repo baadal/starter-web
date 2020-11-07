a11a19fc66322f4a0c7fb737d1bd816db49c5007
Abhishek Raj <abhi@raj.me>
2020-11-08 03:58:09 +0530

    Add support for importing css files

--- src/template.ts
+++ src/template.ts
@@ -8,6 +8,7 @@ export const template = () => {
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/x-icon" href="${publicPath}favicon.ico" />
+    <link rel="stylesheet" href="${publicPath}css/style.css">
     <title>My App</title>
   </head>
   <body>
