cc2908a2101981e1d389c91b5da6b3a3b3e35349
Abhishek Raj <abhi@raj.me>
2020-11-15 19:39:12 +0530

    Setup route for css-in-js (emotion) demo page

---
+++ src/components/pages/demo/css-in-js/css-in-js.component.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CssInJsDemo extends React.Component<CssInJsDemoProps, CssInJsDemoState> {
+  render() {
+    return (
+      <>
+        <h2>Demo: CSS-in-JS (emotion)</h2>
+      </>
+    );
+  }
+}
+
+export interface CssInJsDemoProps {}
+
+export interface CssInJsDemoState {}
+
+export default CssInJsDemo;
