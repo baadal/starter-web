8b020f12fb635f735b632684efe54d54ce596aef
Abhishek Raj <abhi@raj.me>
2020-11-15 05:58:58 +0530

    Add postcss plugins: autoprefixer,cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
+const plugins = [
+  'autoprefixer',
+  'postcss-preset-env',
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('cssnano'); // should be last in plugins array
+}
+
 module.exports = {
-  plugins: [
-    'postcss-preset-env',
-  ]
+  plugins
 };
