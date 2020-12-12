010bcfda9cdddc8e24969d67a07154072140e013
Abhishek Raj <abhi@raj.me>
2020-12-13 03:39:24 +0530

    Setup data source paths

--- src/core/routes/routes.ts
+++ src/core/routes/routes.ts
@@ -4,19 +4,24 @@ import { RouteInfo } from 'src/core/models/route.model';
 const routes: StringIndexable<RouteInfo> = {
   about: {
     path: '/about',
+    source: '/v1/data/about',
   },
   cssStylesDemo: {
     path: '/demo/css-styles',
+    source: '/v1/data/demo/css-styles',
   },
   cssInJsDemo: {
     path: '/demo/css-in-js',
+    source: '/v1/data/demo/css-in-js',
   },
   home: {
     path: '/',
+    source: '/v1/data/home',
   },
   default: {
     name: 'NOT_FOUND',
     path: '/*',
+    source: '/v1/data/not-found',
   },
 };
 
