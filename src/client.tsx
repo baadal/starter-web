011447cd8231b398ee1330e1070e123be2bc8e4e
Abhishek Raj <abhi@raj.me>
2020-12-30 00:11:34 +0530

    Add polyfills for react: Map, Set

--- src/client.tsx
+++ src/client.tsx
@@ -1,3 +1,7 @@
+import 'core-js/es/map'; // For React 16+
+import 'core-js/es/set'; // For React 16+
+import 'raf/polyfill'; // For React 16+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
