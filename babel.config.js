403843e5a31c970fcf4637a8cf75f883ea1c0ef5
Abhishek Raj <abhi@raj.me>
2021-01-01 11:14:20 +0530

    Emit modern (ESNext) JS bundles

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
-// const envUtils = require('./starter/env');
+const envUtils = require('./starter/env');
 
+const isModern = envUtils.checkModern();
 // const isProd = envUtils.checkProd();
 // const isServer = envUtils.checkServer();
 
@@ -11,6 +12,7 @@ module.exports = {
         useBuiltIns: "usage",
         corejs: { version: 3, proposals: true },
         shippedProposals: true,
+        targets: isModern ? { esmodules: true } : {},
         // debug: !isProd && !isServer,
       }
     ],
--- starter/env.ts
+++ starter/env.ts
@@ -1,2 +1,3 @@
 export const checkProd = () => process.env.NODE_ENV === 'production';
 export const checkServer = () => process.env.PLATFORM === 'server';
+export const checkModern = () => process.env.PLATFORM !== 'server' && process.env.MODERN === 'true';
