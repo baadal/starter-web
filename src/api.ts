ced7890920ecf91da89a721ac888a252e8e3695b
Abhishek Raj <abhi@raj.me>
2021-01-07 23:04:23 +0530

    Runtime optimizations: uaParserMap, assetsDataMap, assetsMimeMap, cjsStatsCache

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import express from 'express';
 // @ts-ignore
 import cors from 'cors';
 
-import { getUserAgentInfo } from 'src/ssr/server-utils';
+import { initApiServer, getUserAgentInfo } from 'src/ssr/server-utils';
 import { ServerResponse } from 'src/core/models/response.model';
 
 const app = express();
@@ -181,4 +181,5 @@ app.get('/*', (req, res) => sendResponse(req, res, defaultInfo));
 
 app.listen(PORT, () => {
   console.log(`\nAPI running at port ${PORT} 🎉\n`);
+  initApiServer();
 });
---
+++ src/ssr/server-state.ts
@@ -0,0 +1,4 @@
+export const uaParserMap = new Map<string, string>();
+export const assetsDataMap = new Map<string, string>();
+export const assetsMimeMap = new Map<string, string>();
+export const cjsStatsCache = new Map<string, any>();
