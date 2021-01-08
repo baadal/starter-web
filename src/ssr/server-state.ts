0bcc1f70e2b2ad638ebb759c5c562e5ce73330b0
Abhishek Raj <abhi@raj.me>
2021-01-08 09:55:05 +0530

    Runtime optimizations (esm): esmStatsCache, cjsToEsmMap

--- src/ssr/server-state.ts
+++ src/ssr/server-state.ts
@@ -2,4 +2,6 @@ export const uaParserMap = new Map<string, string>();
 export const assetsDataMap = new Map<string, string>();
 export const assetsMimeMap = new Map<string, string>();
 export const cjsStatsCache = new Map<string, any>();
+export const esmStatsCache = new Map<string, any>();
 export const etcStatsCache = new Map<string, any>();
+export const cjsToEsmMap = new Map<string, string>();
