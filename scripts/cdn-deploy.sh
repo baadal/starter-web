a4d3d79f2f47cdfb6278d6517393b3e4a1d296d0
Abhishek Raj <abhi@raj.me>
2021-01-18 01:31:23 +0530

    temp2

--- scripts/cdn-deploy.sh
+++ scripts/cdn-deploy.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-aws s3 cp build/public s3://$S3_BUCKET_NAME/$INSTANCE_REGION_ALIAS/ --recursive --cache-control "max-age=2592000,public" --acl "public-read" --exclude "*.gz" --exclude "*.br" --metadata origin=$INSTANCE_REGION
+
+aws s3 cp build/public "s3://$S3_BUCKET_NAME/$INSTANCE_REGION_ALIAS/" --recursive --cache-control "max-age=2592000,public" --acl "public-read" --exclude "*.gz" --exclude "*.br" --metadata origin="$INSTANCE_REGION"
+
+if [ "us-east-1" = "$INSTANCE_REGION" ]; then
+  echo "------------------"
+  aws s3 cp build/public "s3://$S3_BUCKET_DEFAULT/" --recursive --cache-control "max-age=2592000,public" --acl "public-read" --exclude "*.gz" --exclude "*.br" --metadata origin="$INSTANCE_REGION"
+fi
