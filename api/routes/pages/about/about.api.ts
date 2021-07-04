1d9ac987dfa8fecf92b6ddee0bd2693578c41596
Abhishek Raj <abhi@raj.me>
2021-07-04 14:41:11 +0530

    Setup API backend [local]

---
+++ api/routes/pages/about/about.api.ts
@@ -0,0 +1,8 @@
+export const aboutInfo = {
+  title: 'About',
+  description: 'React starter kit for building high-performance Web Apps.',
+  seo: {
+    title: 'About Starter.js',
+    description: 'React starter kit for building high-performance Web Apps',
+  },
+};
---
+++ api/routes/pages/home/home.api.ts
@@ -0,0 +1,8 @@
+export const homeInfo = {
+  title: 'React Starter Kit',
+  description: 'Start Building!',
+  seo: {
+    title: 'Starter.js',
+    description: 'A modern way of building Web Apps',
+  },
+};
