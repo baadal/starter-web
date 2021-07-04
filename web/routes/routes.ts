4cfe7bf04115481890e6d6e97482bbc7fa90c74f
Abhishek Raj <abhi@raj.me>
2021-07-04 14:44:16 +0530

    Setup data source paths

--- web/routes/routes.ts
+++ web/routes/routes.ts
@@ -4,13 +4,16 @@ import { StringIndexable } from 'model/common.model';
 const routes: StringIndexable<RouteInfo> = {
   about: {
     path: '/about',
+    source: '/v1/data/about',
   },
   home: {
     path: '/',
+    source: '/v1/data/home',
   },
   default: {
     name: 'NOT_FOUND',
     path: '/*',
+    source: '/v1/data/not-found',
   },
 };
 
