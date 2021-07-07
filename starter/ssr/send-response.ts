f4e9f30cc867dcf8a49d044fd69fc817fbea21e1
Abhishek Raj <abhi@raj.me>
2021-07-07 17:36:24 +0530

    Disallow caching of html pages

--- starter/ssr/send-response.ts
+++ starter/ssr/send-response.ts
@@ -2,5 +2,17 @@ import { Request, Response } from 'express';
 
 export const sendResponse = (response: string, res: Response, req: Request, contentType = 'text/html') => {
   res.header('Content-Type', contentType);
+
+  // disallow caching of html pages
+  if (contentType === 'text/html') {
+    // Browser back/forward navigations retrieve stuff from the cache without revalidation.
+    // Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=516846
+    // res.setHeader('Cache-Control', 'no-cache');
+
+    // Ref: https://dev.to/jamesthomson/spas-have-your-cache-and-eat-it-too-iel
+    // Ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#preventing_caching
+    res.setHeader('Cache-Control', 'no-store, max-age=0');
+  }
+
   res.send(response);
 };
